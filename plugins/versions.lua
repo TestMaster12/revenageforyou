57e1d9bde92990989a29ad5949796ad54071d474
mahrap <ahmadlashgari.al@gmail.com>
2016-01-03 11:24:34 +0330

    Create versions.lua

---
+++ plugins/versions.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return 'critech security '.. VERSION .. [[ 
+ bia👍]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
